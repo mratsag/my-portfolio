09f6d12fe2e32cc186e7562b9fcc396771dc3381
Murat <mrat.sag@hotmail.com>
2025-08-19 15:57:22 +0300

    pagee update

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -90,11 +90,9 @@ export default function RootLayout({
       <head>
         {/* Favicon */}
         <link rel="icon" type="image/svg+xml" href="/favicon.svg" />
-        <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png" />
         <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png" />
-        <link rel="icon" type="image/png" sizes="192x192" href="/favicon.png" />
         <link rel="apple-touch-icon" sizes="180x180" href="/favicon-32x32.png" />
-        <link rel="shortcut icon" href="/favicon.ico" />
+        <link rel="shortcut icon" href="/favicon.svg" />
         <link rel="manifest" href="/manifest.json" />
         <meta name="theme-color" content="#6366f1" />
         <meta name="msapplication-TileColor" content="#6366f1" />
